dd67b1f6e3e90c9ed1a2907f512c03cc251662f8
Jan Kohlíček <kohlicekjan@gmail.com>
2016-11-22 18:46:36 +0100

    Odstranění testů

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name = "boiler-controller",
-      version ="0.7dev",
-      description='',
+      version ="0.9dev",
+      description='Simulace boileru, pomocí konečného automatu',
       author = "Kateřina Kratochvílová,Jan Kohlíček",
       author_email = "dtwok8@students.zcu.cz,kohl@students.zcu.cz",
       url = 'https://github.com/dtwok8/TI-boiler-controller',
-      license = 'BSD',
-      packages = find_packages(exclude=['tests', 'tests.*'])
-      #entry_points = {'console_scripts' : {'main = '}}
-      )
+      license = 'BSD')
